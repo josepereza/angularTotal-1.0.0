7ce593758d129f097f1f6c8cc86f743f5ec60eb4
josepereza <josepereza66@gmail.com>
2020-04-04 09:54:19 +0200

    first1 commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
+
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { IconComponent } from './icon/icon.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    IconComponent,
+    FooterComponent,
+    ContactusComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPageScrollCoreModule,
+    NgxPageScrollModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
